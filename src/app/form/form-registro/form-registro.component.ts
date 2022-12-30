8458fdb03039851e1cca5e11b1f6dbfd6e62b1f7
Emerson Suarez <emersonsuarez2904@gmail.com>
2022-12-30 11:11:47 -0500

    Añadido método guardar registros

--- src/app/form/form-registro/form-registro.component.ts
+++ src/app/form/form-registro/form-registro.component.ts
@@ -15,11 +15,18 @@ export class FormRegistroComponent implements OnInit {
   nombre : string = "";
   apellidos : string = "";
   edad : string = "";
-  registros : string[] = [];
+  registros : any = [];
+
+  guardarDatos(name:string, surname:string, age:string){
+    let validar =  Number(age) >= 18 ? name +` es Mayor de Edad` : name +` es Menor de Edad` ;
+    this.registros.push({name,surname,age,validar});
+  }
 
   obtenerDatos(name:string, surname:string, age:string){
-      this.nombre = name,
+      this.nombre = name;
       this.apellidos = surname;
       this.edad = age;
+
+      this.guardarDatos(this.nombre,this.apellidos, this.edad);
   }
 }
